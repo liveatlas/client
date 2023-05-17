273ed9cafcefdffce9913519661f4361dd957e31
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-05-17 15:09:52 +0900

    chore: introduce clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![deny(clippy::all, unsafe_code)]
+#![warn(clippy::pedantic, clippy::nursery)]
+// print functions do not work on wasm (it compiles, but is ignored silently)
+#![cfg_attr(
+    all(target_family = "wasm", not(test)),
+    deny(clippy::print_stderr, clippy::print_stdout, clippy::print_literal)
+)]
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
